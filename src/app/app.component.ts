c8b69ad5c482799e732dc313a4d6a52b48579bac
VuagnatV <37271745+VuagnatV@users.noreply.github.com>
2024-04-08 22:35:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+//import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
+import { Auth, EmailAuthProvider } from '@angular/fire/auth';
+import firebase from 'firebase/compat';
+import 'firebaseui/dist/firebaseui.css';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sell-my-ride-public';
+
+  //ui!: firebaseui.auth.AuthUI;
+
+  constructor(private auth: Auth) {}
+
+  ngOnInit(): void {
+    const uiConfig: firebaseui.auth.Config = {
+      signInOptions: [EmailAuthProvider.PROVIDER_ID],
+      signInSuccessUrl:
+        'https://firebase.google.com/docs/auth/web/firebaseui?hl=fr',
+    };
+
+    const ui = new firebaseui.auth.AuthUI(this.auth);
+
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'sellmyride-408da',
+          appId: '1:43788889093:web:9e8009a242d34fc179006c',
+          storageBucket: 'sellmyride-408da.appspot.com',
+          apiKey: 'AIzaSyDNUjtF6I5eHpG_6gu_5jzzEaEbFBJhwzw',
+          authDomain: 'sellmyride-408da.firebaseapp.com',
+          messagingSenderId: '43788889093',
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+  ],
 };
